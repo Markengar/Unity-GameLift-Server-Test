0ab03bfc143ff5062eeb7f16ada4279f7f591516
Markengar <maxmartn9@gmail.com>
2022-01-19 02:12:39 +0200

    Adding the Main Project

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class Startup : MonoBehaviour
+{
+    // Helper function for getting the command line arguments
+    // src: https://stackoverflow.com/a/45578115/1956540
+    public static bool IsArgFlagPresent(string name)
+    {
+        var args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == name)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
